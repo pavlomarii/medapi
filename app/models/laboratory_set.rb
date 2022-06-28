4e0585d6626cc216e17b76a29de6503792933af1
Pavlo-Marii <pmarii@cybercraftinc.com>
2022-06-28 22:52:07 +0300

    Finished

--- app/models/laboratory_set.rb
+++ app/models/laboratory_set.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: laboratory_sets
+#
+#  id                    :bigint           not null, primary key
+#  patient_id            :bigint           not null
+#  erythrocytes          :float
+#  hemoglobin            :float
+#  hematocrit            :float
+#  platelets             :float
+#  platelet              :float
+#  leukocytes            :float
+#  lymphocytes           :float
+#  lymphocytes_percents  :float
+#  monocytes             :float
+#  monocytes_percents    :float
+#  shoe                  :float
+#  granulocytes          :float
+#  granulocytes_percents :float
+#  yuni                  :float
+#  reticulocytes_sticks  :float
+#  segments              :float
+#  eosinophils           :float
+#  basophils             :float
+#  reticulocytes         :float
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+# Indexes
+#
+#  index_laboratory_sets_on_patient_id  (patient_id)
+#
 class LaboratorySet < ApplicationRecord
   belongs_to :patient
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,26 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: patients
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  email      :string
+#  phone      :string
+#  blood_type :integer
+#  rhesus     :boolean
+#  user_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_patients_on_email    (email)
+#  index_patients_on_name     (name)
+#  index_patients_on_phone    (phone)
+#  index_patients_on_user_id  (user_id)
+#
 class Patient < ApplicationRecord
   belongs_to :user
   has_many :laboratory_sets, dependent: :destroy
